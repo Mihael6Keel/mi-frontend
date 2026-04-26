d2460c02a505827759fd2c2a28ffc730e805c77f
Angel <fisiangel.14@gmail.com>
2026-04-26 15:40:48 -0500

    add interceptor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,5 @@ import { authGuard } from './core/guards/auth-guard';
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: Login },
-    { path: 'home', component: Home, canActivate: [authGuard] },
+    { path: 'home', component: Home},
 ];
--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,17 +1,32 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('access');
+  const router = inject(Router);
+
+  let clonedReq = req;
 
   if (token) {
-    const clonedReq = req.clone({
+    clonedReq = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
-
-    return next(clonedReq);
   }
 
-  return next(req);
+  return next(clonedReq).pipe(
+    catchError((error) => {
+
+      if (error.status === 401) {
+        localStorage.clear();
+        router.navigate(['/login']);
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
