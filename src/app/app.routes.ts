f74377feaa879a7753370779d2b0fbeb6dfea1bc
Brad Coding <mijhael.unmsm@gmail.com>
2026-04-28 01:00:20 -0500

    Subo mis cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,16 @@ import { Login } from './auth/pages/login/login';
 import { Home } from './pages/home/home';
 import { Landing } from './pages/landing/landing';
 import { authGuard } from './core/guards/auth-guard';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
-  { path: 'landing', component: Landing }, // 🔥 TU HOMEPAGE PRIMERO
+  { path: 'landing', component: Landing }, // HOMEPAGE PRIMERO
   { path: 'login', component: Login },
-  { path: 'home', component: Home }
+  { path: 'home', component: Home },
+  { path: 'dashboard', component: Dashboard },
+  // RUTAS VACÍAS (placeholder)
+  { path: 'solicitudes', component: Dashboard },
+  { path: 'inventario', component: Dashboard },
+  { path: 'ordenes', component: Dashboard },
+  { path: 'recepcion', component: Dashboard },
 ];
